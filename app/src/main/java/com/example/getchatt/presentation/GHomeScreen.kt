75684a33ddc66eca67f5a403e4bd056f6a914b5f
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-23 17:00:55 +0530

    Get Chatting

--- app/src/main/java/com/example/getchatt/presentation/GHomeScreen.kt
+++ app/src/main/java/com/example/getchatt/presentation/GHomeScreen.kt
@@ -1,22 +1,24 @@
 package com.example.getchatt.presentation
 
-import android.text.Layout
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.*
 import com.example.getchatt.R
-import com.example.getchatt.ui.theme.Black
 import com.example.getchatt.ui.theme.RoyalBlue
+import com.example.getchatt.ui.theme.White
 
 @Composable
 fun GHomeScreen() {
@@ -25,7 +27,7 @@ fun GHomeScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(Color.Black)
     ) {
         Image(
             painter = painterResource(id = R.drawable.gchatticon),
@@ -36,14 +38,42 @@ fun GHomeScreen() {
                 )
                 .width(LocalConfiguration.current.screenWidthDp.dp - 40.dp)
         )
-
-        Button(onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(LocalConfiguration.current.screenHeightDp.dp / 8),
-            colors = ButtonDefaults.buttonColors(backgroundColor = RoyalBlue)
+        Row( modifier = Modifier
+            .fillMaxWidth()
+            .height(LocalConfiguration.current.screenHeightDp.dp / 9)
+            .background(color = White),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-
+            Text(
+                text = "Let's Get Started",
+                color = RoyalBlue,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = LocalConfiguration.current.fontScale.times(25).sp
+                )
+            )
         }
     }
+}
+
+@Composable
+private fun ChattingGuys () {
+
+    val compositionResult: LottieCompositionResult = rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(
+            R.raw.chatting
+
+        )
+    )
+
+    val progress by animateLottieCompositionAsState(
+        compositionResult.value,
+        isPlaying = true,
+        iterations = LottieConstants.IterateForever,
+        speed = 1.0f
+    )
+
+    LottieAnimation(compositionResult.value, progress)
+
 }
