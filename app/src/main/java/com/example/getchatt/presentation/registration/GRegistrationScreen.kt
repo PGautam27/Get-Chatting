3a354c3ecc63e8566703c7c5d3e53fbee958c915
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-06-06 15:20:31 +0530

    Get Chatting

--- app/src/main/java/com/example/getchatt/presentation/registration/GRegistrationScreen.kt
+++ app/src/main/java/com/example/getchatt/presentation/registration/GRegistrationScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +16,11 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,18 +33,18 @@ import com.example.getchatt.ui.theme.White
 fun GRegistrationScreen() {
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
     val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
-    BottomSheetScaffold(sheetContent = { register() }, content = { mainContent()}, sheetShape = (if (sheetState.isAnimationRunning || sheetState.isVisible){
+    BottomSheetScaffold(sheetContent = { register() }, content = { Images()}, sheetShape = (if (sheetState.isAnimationRunning || sheetState.isVisible){
         RoundedCornerShape(topStart = 0.dp, topEnd = 0.dp)
     }else{
-        RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp)
+        RoundedCornerShape(topStart = 60.dp, topEnd = 60.dp)
     }) as Shape,
         scaffoldState = bottomSheetScaffoldState,
-        sheetPeekHeight = if (sheetState.isAnimationRunning || sheetState.isVisible) 0.dp else 200.dp
+        sheetPeekHeight = if (sheetState.isAnimationRunning || sheetState.isVisible) 0.dp else 180.dp
     )
 }
 
 @Composable
-private fun mainContent() {
+private fun Images() {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -61,16 +67,63 @@ private fun register() {
     val emailValue = remember {
         mutableStateOf("")
     }
+    val passwordValue = remember {
+        mutableStateOf("")
+    }
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxSize()
+            .height(LocalConfiguration.current.screenHeightDp.dp/2 + 50.dp)
             .background(RoyalBlue),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Enter you email and \n set a password", textAlign = TextAlign.Center, style = TextStyle(color = White, fontSize = LocalConfiguration.current.fontScale.times(25).sp))
-        Spacer(modifier = Modifier.padding(LocalConfiguration.current.screenHeightDp.dp/10))
-        OutlinedTextField(value = emailValue.value, onValueChange = {emailValue.value = it})
+
+        Text(text = "Let's Get you Registered", textAlign = TextAlign.Center, style = TextStyle(color = White, fontSize = LocalConfiguration.current.fontScale.times(28).sp))
+        Spacer(modifier = Modifier.padding(LocalConfiguration.current.screenHeightDp.dp/30))
+        Text(text = "Enter your email and \n set a password", textAlign = TextAlign.Center, style = TextStyle(color = White, fontSize = LocalConfiguration.current.fontScale.times(25).sp))
+        Spacer(modifier = Modifier.padding(LocalConfiguration.current.screenHeightDp.dp/30))
+        OutlinedTextField(
+            value = emailValue.value,
+            placeholder = { Text(text = "xyz@gmail.com", style = TextStyle(fontSize = 16.sp, color = White))},
+            onValueChange = { emailValue.value = it },
+            label = { Text(text = "EMAIL ID", style = TextStyle( fontWeight = FontWeight.Bold, fontSize = LocalConfiguration.current.fontScale.times(18).sp))},
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.White,
+                focusedLabelColor = Color.White,
+                unfocusedLabelColor = Color.White,
+                cursorColor = Color.White,
+                textColor = Color.White,
+                leadingIconColor = Color.White
+            ),
+            textStyle = TextStyle( fontSize = 16.sp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            ),
+            modifier = Modifier.width(LocalConfiguration.current.screenWidthDp.dp - 80.dp)
+        )
+        OutlinedTextField(
+            value = passwordValue.value,
+            onValueChange = { passwordValue.value = it },
+            label = { Text(text = "PASSWORD", style = TextStyle( fontWeight = FontWeight.Bold, fontSize = LocalConfiguration.current.fontScale.times(18).sp))},
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.White,
+                focusedLabelColor = Color.White,
+                unfocusedLabelColor = Color.White,
+                cursorColor = Color.White,
+                textColor = Color.White,
+                leadingIconColor = Color.White
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            textStyle = TextStyle( fontSize = 16.sp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            modifier = Modifier.width(LocalConfiguration.current.screenWidthDp.dp - 80.dp)
+        )
     }
 }
